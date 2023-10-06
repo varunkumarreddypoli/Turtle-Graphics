47cbc2324286d2c8232a4a42f22ebfbbc1081fc6
varunkumarreddypoli <137781983+varunkumarreddypoli@users.noreply.github.com>
2023-10-06 11:28:56 +0530

    Add files via upload

---
+++ Turtle alphabets.py
@@ -0,0 +1,555 @@
+
+#a
+import turtle
+turtle.color('green')
+turtle.pensize(15)
+turtle.forward(1)
+turtle.left(78)
+turtle.forward(100)
+turtle.right(150)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(40)
+turtle.clearscreen()
+
+# alpha B
+from turtle import *
+bgcolor("black")
+speed(6)
+color("white")
+width(16)
+fd(70)
+rt(40)
+for n in range(14):
+	fd(9)
+	rt(10)
+lt(150)
+fd(20)
+for n in range(14):
+	fd(10)
+	rt(10)
+rt(11)
+fd(60)
+rt(90)
+fd(213)
+rt(90)
+color("white")
+clearscreen()
+
+# c
+import turtle
+turtle.color('pink')
+turtle.pensize(15)
+turtle.forward(20)
+turtle.left(20)
+turtle.forward(20)
+turtle.right(180)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.right(20)
+turtle.forward(20)
+turtle.clearscreen()
+
+# alphabet D
+from turtle import*
+fd(2)
+color("red")
+pensize(7)
+speed(4)
+for n in range(22):
+	fd(6)
+	rt(8)
+fd(10)
+rt(90)
+fd(100)
+clearscreen()
+
+# alpha E
+from turtle import*
+hideturtle()
+seth(00)
+color('gold')
+pensize(5)
+bk(100)
+lt(90)
+fd(100)
+rt(90)
+fd(100)
+bk(100)
+lt(90)
+fd(100)
+rt(90)
+fd(100)
+clearscreen()
+
+
+# alphabet F
+from turtle import*
+bgcolor("black")
+speed(1)
+color("yellow")
+pensize(9)
+lt(90)
+fd(200)
+rt(90)
+fd(100)
+bk(100)
+bgcolor("white")
+begin_fill()
+end_fill()
+rt(90)
+fd(70)
+lt(90)
+fd(100)
+clearscreen()
+
+# alpha G
+from turtle import*
+hideturtle()
+seth(180)
+color("blue")
+pensize(5)
+fd(40)
+circle(80,260)
+lt(100)
+fd(90)
+bk(120)
+lt(90)
+fd(80)
+clearscreen()
+
+#h
+import turtle 
+turtle.pensize(6)
+turtle.color("red")
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(20)
+turtle.left(90)
+turtle.forward(220)
+turtle.right(180)
+turtle.forward(225)
+turtle.clearscreen()
+
+
+# alpha I
+from turtle import*
+color("cyan")
+pensize(5)
+fd(100)
+lt(90)
+fd(70)
+lt(90)
+fd(70)
+rt(90)
+fd(100)
+rt(90)
+fd(70)
+lt(90)
+fd(70)
+lt(90)
+fd(100)
+lt(90)
+fd(70)
+lt(90)
+fd(70)
+rt(90)
+fd(100)
+rt(90)
+fd(70)
+lt(90)
+fd(70)
+clearscreen()
+
+# alpha J
+from turtle import*
+hideturtle()
+pensize(5)
+color('violet')
+seth(00)
+bk(190)
+fd(95)
+rt(90)
+fd(150)
+for n in range(26):
+	rt(8)
+	fd(10)
+clearscreen()
+
+# alpha k
+from turtle import*
+hideturtle()
+pensize(5)
+color("gray")
+seth(90)
+fd(200)
+bk(100)
+rt(130)
+fd(140)
+bk(140)
+lt(90)
+fd(120)
+clearscreen()
+
+# alpha L
+from turtle import*
+pensize(5)
+color("gray")
+fd(180)
+lt(90)
+fd(50)
+lt(90)
+fd(100)
+rt(90)
+fd(150)
+lt(90)
+fd(80)
+lt(90)
+fd(150)
+clearscreen()
+
+# alpha M
+from turtle import*
+hideturtle()
+pensize(5)
+color("purple")
+seth(90)
+fd(100)
+rt(140)
+fd(65)
+lt(100)
+fd(65)
+rt(140)
+fd(100)
+clearscreen()
+
+# alpha N
+from turtle import*
+hideturtle()
+pensize(5)
+color("lime")
+seth(90)
+fd(100)
+rt(140)
+fd(130)
+lt(140)
+fd(100)
+clearscreen()
+
+
+#letter O
+
+import turtle
+t=turtle
+for n in range(25):
+ t.pensize(15)
+ t.color('orange')
+ t.speed(5)
+ t.forward(29)
+ t.right(19)
+ t.forward(18)
+ t.right(18)
+t.clearscreen()
+
+# alpha p
+from turtle import*
+hideturtle()
+pensize(5)
+color("olive")
+seth(90)
+fd(200)
+rt(90)
+for n in range(19):
+	fd(10)
+	rt(10)
+clearscreen()
+
+# alpha Q
+from turtle import*
+hideturtle()
+circle(100,410)
+pensize(5)
+color("blue")
+pu()
+color("blue")
+lt(90)
+fd(30)
+pd()
+bk(90)
+clearscreen()
+
+# alpha R
+from turtle import*
+hideturtle()
+pensize(5)
+color("brown")
+seth(90)
+fd(200)
+rt(90)
+for n in range(19):
+	fd(10)
+	rt(10)
+lt(140)
+fd(100)
+clearscreen()
+
+#alpha s
+from turtle import*
+pensize(5)
+color("navy")
+width(8)
+color("black")
+for i in range(9):
+	fd(20)
+	lt(20)
+for i in range(9):
+	fd(20)
+	rt(20)
+fd(44)
+clearscreen()
+
+# T
+from turtle import*
+pensize(5)
+color("indigo")
+fd(1)
+lt(90)
+fd(180)
+lt(90)
+fd(100)
+bk(200)
+clearscreen()
+
+# alpha u
+from turtle import*
+pensize(5)
+color("salmon")
+seth(90)
+width(2)
+fd(100)
+rt(180)
+fd(100)
+width(6)
+for n in range(16):
+	fd(30)
+	width(6)
+	lt(12)
+rt(12)
+fd(100)
+clearscreen()
+
+# alpha v
+from turtle import*
+hideturtle()
+pensize(5)
+color("cyan")
+seth(0)
+fd(1)
+rt(70)
+fd(250)
+lt(140)
+fd(250)
+rt(75)
+fd(40)
+rt(105)
+fd(280)
+rt(70)
+fd(40)
+rt(70)
+fd(280)
+rt(105)
+fd(20)
+clearscreen()
+
+#alpha W
+import turtle
+t=turtle
+t.speed(2)
+t.bgcolor("black")
+t.color("red")
+t.fillcolor("yellow")
+t.pensize(9)
+t.forward(1)
+t.right(75)
+t.forward(200)
+t.color("white")
+t.left(155)
+t.forward(200)
+t.right(155)
+t.forward(200)
+t.left(155)
+t.forward(200)
+t.right(85)
+t.forward(30)
+t.right(95)
+t.forward(227)
+t.right(89)
+t.forward(39)
+t.right(60)
+t.forward(100)
+t.left(150)
+t.forward(100)
+t.right(90)
+t.forward(39)
+t.right(65)
+t.color("red")
+t.forward(220)
+t.right(115)
+t.forward(32)
+t.clearscreen()
+
+
+# alpha X
+
+
+import turtle
+t=turtle
+t.bgcolor("black")
+t.speed(1)
+t.color("black")
+t.pensize(5)
+t.color("red","white")
+t.forward(1)
+t.right(40)
+t.forward(90)
+t.pensize(5)
+t.color("yellow")
+t.forward(90)
+t.right(180)
+t.forward(90)
+t.right(90)
+t.forward(90)
+t.right(180)
+t.forward(90)
+t.pensize(5)
+t.color("red")
+t.forward(90)
+t.clearscreen()
+
+
+
+
+# Alpha Y
+
+
+import turtle
+t=turtle
+t.speed(2)
+t.bgcolor("black")
+t.forward(1)
+t.right(50)
+t.pensize(15)
+t.color("orange")
+t.forward(100)
+t.left(110)
+t.color("blue")
+t.pensize(5)
+t.forward(100)
+t.right(78)
+t.forward(30)
+t.right(100)
+t.forward(220)
+t.right(90)
+t.forward(30)
+t.right(90)
+t.forward(125)
+t.color("orange")
+t.clearscreen()
+
+
+# alpha Z
+import turtle
+t=turtle
+t.speed(3)
+t.pensize(8)
+t.bgcolor("teal")
+t.color("white")
+t.forward(180)
+t.right(140)
+t.forward(250)
+t.left(140)
+t.forward(189)
+for n in range(6):
+	t.color('black')
+	t.forward(6)
+	t.right(30)
+t.forward(30)
+t.color("white")	
+t.forward(40)
+t.color("yellow")
+t.forward(5)
+t.color("red")
+t.forward(8)
+t.color("orange")
+t.forward(90)
+t.color("pink")
+t.forward(39)
+for n in range(4):
+	t.forward(6)
+	t.color("black")
+	t.right(30)
+t.right(20)
+t.color("white")
+t.forward(210)
+for n in range(5):
+	t.forward(6)
+	t.left(30)
+t.forward(20)
+t.right(10)
+t.color("pink")
+t.forward(30)
+t.color("yellow")
+t.forward(30)
+t.color("violet")
+t.forward(40)
+t.color("indigo")
+t.forward(7)
+for n in range(5):
+	t.forward(5)
+	t.right(30)
+t.right(30)
+t.color("white")
+t.forward(24)
+t.clearscreen()
+write('VARUN KUMAR REDDY POLI',font=('Arial',10))
+exitonclick()
+
